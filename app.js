f27086081733c973b6648e0b9135d1b517bc263b
radoneje <den.shevchenko@gmail.com>
2019-10-29 19:48:08 +0300

    ee

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.listen(8080, function () {
       infoCoder=spawn("/usr/bin/ffmpeg", [
           "-f", "x11grab",
           "-video_size", "1920x1080",
-          "-i", ":98.7",
+          "-i", ":97.0",
           "-codec:v", "libx264",
           '-r', '30', "-preset", "ultrafast",
           "-y", "/var/www/rasp/public/1.mp4",
